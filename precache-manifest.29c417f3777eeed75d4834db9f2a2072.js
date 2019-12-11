ed181dd7f231e633bd9a272d28a07db61b5f18d7
Michelle Rogers <michelleevarogers@gmail.com>
2019-12-10 16:54:30 -0800

    Updates

---
+++ precache-manifest.29c417f3777eeed75d4834db9f2a2072.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98abe8cdbbd0810e3828d99007f7c6f9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8c67e0754cf391950cbb",
+    "url": "/static/css/main.270d32d7.chunk.css"
+  },
+  {
+    "revision": "e9b5639b6573dc598d2c",
+    "url": "/static/js/2.965fc5b4.chunk.js"
+  },
+  {
+    "revision": "8c67e0754cf391950cbb",
+    "url": "/static/js/main.1d06a3ee.chunk.js"
+  },
+  {
+    "revision": "ecd96604726d18c0d931",
+    "url": "/static/js/runtime-main.78169804.js"
+  },
+  {
+    "revision": "02b0757488cc0aaca790353423ca9ec3",
+    "url": "/static/media/logo.02b07574.png"
+  }
+]);
